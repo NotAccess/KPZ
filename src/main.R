86c25c6801922e1072fc6507c65bf846162028f5
NotAccess <62353774+NotAccess@users.noreply.github.com>
2025-03-01 14:04:43 +0300

    Update main.R

--- src/main.R
+++ src/main.R
@@ -2,7 +2,7 @@ source("src/GetData.R")
 
 # Пример вызова функции
 username <- "NotAccess"
-repos <- list("KPZ", "Iatp", "TMP", "thdataimport") # Нужна функция для парсинга имён репозиториев
+repos <- get_github_repositories(username)
 for (repo in repos) {
   logs <- clone_and_log_repository(username, repo)
   print(logs) # Вывод для теста
